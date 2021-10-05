848fe816e931c5237b884f56c5b10090879adaeb
Harold Burbano <haroldburbano2002@gmail.com>
2021-10-04 21:47:55 -0300

    Start working in javascript

---
+++ index.js
@@ -0,0 +1,24 @@
+const cleanButton = document.getElementById("cleanButton")
+const countButton = document.getElementById("countButton")
+const RADIO_1  = document.getElementById("radio-1")
+const RADIO_2 = document.getElementById("radio-2")
+const RADIO_3 = document.getElementById("radio-3")
+const FILTER_INPUT = document.getElementById("filter-text")
+const WORD_INPUT = document.getElementById("texarea")
+const RESULT_1 = document.getElementById("result-1")
+const RESULT_2 = document.getElementById("result-2")
+
+countButton.addEventListener("click", ()=>
+{
+    const texto = WORD_INPUT.value
+    if(RADIO_1.checked)
+    {
+        
+    }else if(RADIO_2.checked)
+    {
+
+    }else
+    {
+
+    }
+})
